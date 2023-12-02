348a0ac6ae6798380ef30750e5bb3f106ef25b6b
DorraBenMimoun <dorrabenmimoun@gmail.com>
2023-12-02 18:21:02 +0100

    02/12

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const socketIo = require('socket.io');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Remplacez par l'URL de votre application React
+    origin: 'http://localhost:3002', // Remplacez par l'URL de votre application React
     credentials: true,
   }));
 
